ff6089424a122806af8a29461a7cdc273f5afbaa
dannyk-dev <kruger.dkk@gmail.com>
2025-08-21 12:57:10 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -25,20 +25,6 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
     ],
   },
-  // Ensure proper handling of Vercel Analytics and Speed Insights
-  // headers: async () => {
-  //   return [
-  //     {
-  //       source: '/_vercel/speed-insights/script.js',
-  //       headers: [
-  //         {
-  //           key: 'Cache-Control',
-  //           value: 'public, max-age=31536000, immutable',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
